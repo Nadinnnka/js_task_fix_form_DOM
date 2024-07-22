a94d3d7e651156752a97eda57aafbed996c4cea5
Nadinnnka <tymoshyk02@gmail.com>
2024-07-22 11:47:13 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,15 @@
+/* eslint-disable prettier/prettier */ "use strict";
+const inputs = [
+    ...document.querySelectorAll("input")
+];
+inputs.forEach((input)=>{
+    const label = document.createElement("label");
+    label.classList.add("field-label");
+    label.setAttribute("for", input.id);
+    const placeholderText = input.name.replace(/([A-Z])/g, " $1").replace(/^./, (str)=>str.toUpperCase());
+    label.textContent = placeholderText;
+    input.setAttribute("placeholder", placeholderText);
+    input.parentElement.insertBefore(label, input);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
